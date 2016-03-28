6b612f72e614e838318f9a1b2b039c705a230479
Darnell Gillead <dlgillead@gmail.com>
2016-03-28 15:13:35 -0700

    Update myStack.h

--- myStack.h
+++ myStack.h
@@ -9,42 +9,41 @@ template <class Type>
 class myStack
 {
 private:
-	int maxStackSize;														// Variable to store maximum stack size.
-	int stackTop;															// Variable to point to the top of the stack.
-	Type* list;																// Pointer to the array that holds the stack elements.
+	int maxStackSize;								// Variable to store maximum stack size.
+	int stackTop;									// Variable to point to the top of the stack.
+	Type* list;									// Pointer to the array that holds the stack elements.
 public:
-	const myStack<Type>& operator=(const myStack<Type>&);					// Function to overload assignment operator.
+	const myStack<Type>& operator=(const myStack<Type>&);				// Function to overload assignment operator.
 
-	void initializeStack();													// Function to initialize the stack.
-																			// Postcondition: stackTop = 0;
+	void initializeStack();								// Function to initialize the stack.
+											// Postcondition: stackTop = 0;
 
-	bool isEmptyStack() const;												// Function to determine if the stack is empty.
-																			// Postcondition: returns true if empty, false otherwise.
+	bool isEmptyStack() const;							// Function to determine if the stack is empty.
+											// Postcondition: returns true if empty, false otherwise.
+	bool isFullStack() const;							// Function to determine if stack is full.
+											// Postcondition: returns true if stack is full, false otherwise.
 
-	bool isFullStack() const;												// Function to determine if stack is full.
-																			// Postcondition: returns true if stack is full, false otherwise.
+	void push(const Type& newItem);							// Function to add newItem to the stack.
+											// Postcondition: stack is changed and newItem is added to top of stack.
 
-	void push(const Type& newItem);											// Function to add newItem to the stack.
-																			// Postcondition: stack is changed and newItem is added to top of stack.
+	Type top() const;								// Function to return the top element of the stack.
+											// Postcondition: if stack is empty, the program terminates; otherwise, the top
+											// element of the stack is returned.
 
-	Type top() const;														// Function to return the top element of the stack.
-																			// Postcondition: if stack is empty, the program terminates; otherwise, the top
-																			// element of the stack is returned.
+	void pop();									// Function to remove the top element of the stack.
+											// Postcondition: the stack is changed and the top element is removed from the stack.
 
-	void pop();																// Function to remove the top element of the stack.
-																			// Postcondition: the stack is changed and the top element is removed from the stack.
+	void copyStack(const myStack<Type>& otherStack);				// Function to make a copy of otherStack.
+											// Postcondition: a copy of otherStack is created and assigned to this stack.
 
-	void copyStack(const myStack<Type>& otherStack);						// Function to make a copy of otherStack.
-																			// Postcondition: a copy of otherStack is created and assigned to this stack.
+	myStack(int stackSize = 100);							// Default constructor.
+											// Postcondition: stack is created and set to 100 by default if user enters invalid 
+											// stacksize.
 
-	myStack(int stackSize = 100);											// Default constructor.
-																			// Postcondition: stack is created and set to 100 by default if user enters invalid 
-																			// stacksize.
+	myStack(const myStack<Type>& otherStack);					// Copy constructor.
+											// Postcondition: copy of otherStack is stored in stack.
 
-	myStack(const myStack<Type>& otherStack);								// Copy constructor.
-																			// Postcondition: copy of otherStack is stored in stack.
-
-	~myStack();																// Destructor.
+	~myStack();									// Destructor.
 };
 
 
